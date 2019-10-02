1c6fd71ec9195f5043eb93dde9f30be70a273201
tianhaoz95 <tianhaoz@umich.edu>
2019-10-01 23:07:20 -0700

    dev: add integration test

---
+++ iwfpapp/test_driver/app_test.dart
@@ -0,0 +1,36 @@
+// Imports the Flutter Driver API.
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('iwfpapp', () {
+    // First, define the Finders and use them to locate widgets from the
+    // test suite. Note: the Strings provided to the `byValueKey` method must
+    // be the same as the Strings we used for the Keys in step 1.
+    final shopNavBtn = find.byValueKey('shop_nav_btn');
+    final cardsNavBtn = find.byValueKey('cards_nav_btn');
+    final userNavBtn = find.byValueKey('user_nav_btn');
+    final contribNavBtn = find.byValueKey('contrib_nav_btn');
+
+    FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    test('smoke test', () async {
+      await driver.tap(cardsNavBtn);
+      await driver.tap(userNavBtn);
+      await driver.tap(contribNavBtn);
+      await driver.tap(shopNavBtn);
+    });
+  });
+}
