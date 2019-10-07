fd5fcad4ee70976b482dcebe05a497497ac32893
Tianhao Zhou <tianhaoz@umich.edu>
2019-10-07 10:17:56 -0700

    dev: add user ui (#27)

---
+++ iwfpapp/lib/screens/user/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserSettings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        key: Key('contrib_screen_content'),
+        padding: const EdgeInsets.all(36.0),
+        child: ListView(
+          children: <Widget>[
+            Text('Coming soon... You don\'t have settings anyway lol.',
+                style: TextStyle(color: Colors.black45)),
+          ],
+        ));
+  }
+}
--- iwfpapp/test/user_screen_test.dart
+++ iwfpapp/test/user_screen_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:iwfpapp/user.dart';
+import 'package:iwfpapp/screens/user/main.dart';
 
 void main() {
   testWidgets('test user widget render no crash', (WidgetTester tester) async {
