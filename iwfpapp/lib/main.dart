6bab9ca2b06327e4197b479d065923d21cb08d14
tianhaoz95 <tianhaoz@umich.edu>
2019-10-14 20:35:40 -0700

    dev: add deps for ios and android (ios working)

--- iwfpapp/lib/main.dart
+++ iwfpapp/lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'nav.dart';
 import 'screens/login/main.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
