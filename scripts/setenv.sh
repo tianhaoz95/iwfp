f5dc1f94e114f8137522fdf1c4d394f7be0335ec
tianhaoz95 <tianhaoz@umich.edu>
2019-10-26 20:56:42 -0700

    dev: fix the fucking build

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -3,8 +3,8 @@ rm -f ./iwfpapp/android/key.properties
 echo "storePassword=$ANDROID_SIGN_PWD" >> ./iwfpapp/android/key.properties
 echo "keyPassword=$ANDROID_SIGN_PWD" >> ./iwfpapp/android/key.properties
 echo "keyAlias=key" >> ./iwfpapp/android/key.properties
-echo "storeFile=\"$PROJ/$SECRET_REPO/iwfp/key.jks\"" >> ./iwfpapp/android/key.properties
+echo "storeFile=key.jks" >> ./iwfpapp/android/key.properties
 
 rm -f ./iwfpapp/android/fastlane/Appfile
-echo "json_key_file \"$PROJ/$SECRET_REPO/iwfp/play_store_service_account_api_key.json\"" >> ./iwfpapp/android/fastlane/Appfile
+echo "json_key_file \"$PROJ$SECRET_REPO/iwfp/play_store_service_account_api_key.json\"" >> ./iwfpapp/android/fastlane/Appfile
 echo "package_name \"com.jacksonz.iwfpapp\"" >> ./iwfpapp/android/fastlane/Appfile
