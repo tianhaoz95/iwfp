c5e5deffb23f5e8921e3d9a721b638037bbaf44a
Tianhao Zhou <tianhaoz@umich.edu>
2019-10-27 00:14:06 -0700

    dev: add fastlane config (#51)
    * dev: init fastlane and update app icon
    
    * dev: add fastlane config
    
    * dev: add secret repo
    
    * dev: add repo secret and set env script
    
    * dev: add script running
    
    * dev: include secret in ci
    
    * dev: add key paths
    
    * dev: fix typo
    
    * dev: add bundle build
    
    * dev: add logging
    
    * dev: change env name
    
    * dev: remove logging
    
    * dev: fix the fucking build
    
    * dev: add logging
    
    * dev: debug ci
    
    * dev: add flutter upgrade
    
    * dev: clean up build file
    
    * dev: clean up build script

---
+++ scripts/setenv.sh
@@ -0,0 +1,10 @@
+# This script should run from the root project directory
+rm -f ./iwfpapp/android/key.properties
+echo "storePassword=$ANDROID_SIGN_PWD" >> ./iwfpapp/android/key.properties
+echo "keyPassword=$ANDROID_SIGN_PWD" >> ./iwfpapp/android/key.properties
+echo "keyAlias=key" >> ./iwfpapp/android/key.properties
+echo "storeFile=key.jks" >> ./iwfpapp/android/key.properties
+
+rm -f ./iwfpapp/android/fastlane/Appfile
+echo "json_key_file \"$PROJ$SECRET_REPO/iwfp/play_store_service_account_api_key.json\"" >> ./iwfpapp/android/fastlane/Appfile
+echo "package_name \"com.jacksonz.iwfpapp\"" >> ./iwfpapp/android/fastlane/Appfile
