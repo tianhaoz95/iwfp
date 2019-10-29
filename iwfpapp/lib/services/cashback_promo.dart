5ae00343b88e3babb5666ecc3e08745bd6e63968
Tianhao Zhou <tianhaoz@umich.edu>
2019-10-29 17:28:56 +0000

    dev: add comment on promo repeat types

--- iwfpapp/lib/services/cashback_promo.dart
+++ iwfpapp/lib/services/cashback_promo.dart
@@ -4,6 +4,10 @@ class CashbackPromo {
   final String type;
   final String start;
   final String end;
+  /// repeat indicates the rule for how the promotion repeats.
+  /// supported types are:
+  /// * annual (repeats annually between start and end)
+  /// * const (constant, always the same. In this case, start and end are ignored)
   final String repeat;
   final int rate;
   const CashbackPromo(this.name, this.id, this.type, this.start, this.end,
