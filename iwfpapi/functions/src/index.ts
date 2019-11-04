242bba5068ccf48e190bf60d7443ad0bce4512b3
tianhaoz95 <tianhaoz@umich.edu>
2019-11-04 15:59:51 -0800

    dev: add hello world

--- iwfpapi/functions/src/index.ts
+++ iwfpapi/functions/src/index.ts
@@ -6,3 +6,7 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!\n\n');
+});
