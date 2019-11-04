77cd943c04ba1403e31da6aefc2b5e2357d25a54
tianhaoz95 <tianhaoz@umich.edu>
2019-11-03 18:37:32 -0800

    dev: add go to home btn

---
+++ iwfpapp/lib/widgets/buttons/go_to_home_btn.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+typedef onPressedCallbackFunc = void Function();
+
+class GoToHomeButton extends StatelessWidget {
+  final onPressedCallbackFunc onPressedCallback;
+  const GoToHomeButton({this.onPressedCallback});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        key: Key('go_to_home_btn'),
+        elevation: 0.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.blue,
+        child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () {
+              onPressedCallback();
+            },
+            child: Text('Continue to Home',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white))));
+  }
+}
