6a8b8e11ed20b088bfe8a2e76a3f49230cfedb89
Tianhao Zhou <tianhaoz@umich.edu>
2019-11-05 12:38:36 -0800

    dev: add api server (#72)
    * dev: init default fb functions code
    
    * dev: add hello world
    
    * dev: add node setup for checkin checks
    
    * dev: add api deployment script

---
+++ iwfpapi/functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!\n\n');
+});
