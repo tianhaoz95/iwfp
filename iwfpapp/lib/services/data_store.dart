e2adb00671300a1f28f0cfeb7416a484eb19391d
tianhaoz95 <tianhaoz@umich.edu>
2019-11-10 00:05:51 -0800

    dev: impl data store add card api

--- iwfpapp/lib/services/data_store.dart
+++ iwfpapp/lib/services/data_store.dart
@@ -1,10 +1,44 @@
 import 'package:iwfpapp/services/shop_category.dart';
 import 'package:iwfpapp/services/credit_card.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+
+enum ResponseStatus {
+  SUCCEESS,
+  FAILURE,
+}
+
+class CloudFuncResponse {
+  ResponseStatus status;
+  String errorMsg;
+}
 
 class DataStore {
   List<ShopCategory> categories = [];
   List<CreditCard> cards = [];
   String serviceType;
-  DataStore(this.serviceType);
-  // Future<void> addCard(CreditCard card) async {}
+  HttpsCallable addCardCallable;
+  DataStore(this.serviceType) {
+    addCardCallable = CloudFunctions.instance.getHttpsCallable(
+      functionName: 'addCreditCard',
+    );
+  }
+  Future<CloudFuncResponse> addCard(CreditCard card) async {
+    CloudFuncResponse response;
+    Map<String, dynamic> req;
+    req['cardUid'] = card.id;
+    req['cardData'] = <String, dynamic>{
+      'cardUid': card.id,
+      'cardName': card.name,
+    };
+    try {
+      await addCardCallable.call(req);
+      response.status = ResponseStatus.SUCCEESS;
+      response.errorMsg = 'na';
+      return response;
+    } catch (err) {
+      response.status = ResponseStatus.FAILURE;
+      response.errorMsg = err.toString();
+      return response;
+    }
+  }
 }
