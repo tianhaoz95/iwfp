5c9bbac65c0ae89333eb0b98e678f3f213af10ea
tianhaoz95 <tianhaoz@umich.edu>
2019-11-10 22:31:50 -0800

    dev: add remove card impl

--- iwfpapi/functions/src/handler/remove_credit_card.ts
+++ iwfpapi/functions/src/handler/remove_credit_card.ts
@@ -4,9 +4,21 @@ import { noAuthMsg } from "../config/consts";
 function RemoveCreditCardHandler(data, context) {
   return new Promise((resolve, reject) => {
     if (context.auth) {
-      // todo implement remove card
+      const userUid = context.auth.uid;
+      const userRef = provider.getUserRef(userUid);
+      const cardRef = userRef.collection("cards").doc(data.cardUid);
+      cardRef
+        .delete()
+        .then(() => {
+          resolve();
+        })
+        .catch(err => {
+          reject(err);
+        });
     } else {
       reject(noAuthMsg);
     }
   });
 }
+
+export default RemoveCreditCardHandler;
