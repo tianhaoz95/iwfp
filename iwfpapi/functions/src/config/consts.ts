81c74643715ea1e76103cf723bb893754a57bf55
tianhaoz95 <tianhaoz@umich.edu>
2019-11-11 19:02:46 -0800

    dev: add stub apis

--- iwfpapi/functions/src/config/consts.ts
+++ iwfpapi/functions/src/config/consts.ts
@@ -1,2 +1,4 @@
 export const cardCollectionPath: string = "cards";
 export const noAuthMsg: string = "User not signed in";
+export const creditCardNotExistError: string = "Credit card not exist";
+export const notImplementedError: string = "Not implemented";
