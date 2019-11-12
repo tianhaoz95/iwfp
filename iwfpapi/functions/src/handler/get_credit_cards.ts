b21d4a3f2c843277a000f890a31bd0376a7cf2f6
tianhaoz95 <tianhaoz@umich.edu>
2019-11-12 09:39:17 -0800

    dev: add card getter api

---
+++ iwfpapi/functions/src/handler/get_credit_cards.ts
@@ -0,0 +1,21 @@
+import provider from "../provider";
+import { noAuthMsg } from "../config/consts";
+
+function getCreditCardsHandler(data, context) {
+  return new Promise((resolve, reject) => {
+    if (context.auth) {
+      const userUid = context.auth.uid;
+      const userRef = provider.getUserRef(userUid);
+      const cardRef = userRef.collection("cards");
+      cardRef.get().then((snap) => {
+        resolve(snap);
+      }).catch((err) => {
+        reject(err);
+      });
+    } else {
+      reject(noAuthMsg);
+    }
+  });
+}
+
+export default getCreditCardsHandler;
