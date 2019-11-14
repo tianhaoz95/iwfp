1c2af7760319b4e3cf313458c51eed5edc066a25
tianhaoz95 <tianhaoz@umich.edu>
2019-11-13 23:04:26 -0800

    dev: fix tests

---
+++ iwfpapp/test/integration/mock_auth.dart
@@ -0,0 +1,4 @@
+import 'package:iwfpapp/services/auth.dart';
+import 'package:mockito/mockito.dart';
+
+class MockIwfpappAuth extends Mock implements IwfpappAuth {}
--- iwfpapp/test/integration/mock_data_store.dart
+++ iwfpapp/test/integration/mock_data_store.dart
@@ -1,4 +1,10 @@
+import 'package:iwfpapp/services/credit_card.dart';
 import 'package:iwfpapp/services/data_store.dart';
 import 'package:mockito/mockito.dart';
 
-class MockDataStore extends Mock implements DataStore {}
+class MockDataStore extends Mock implements DataStore {
+  @override
+  Future<List<CreditCard>> getCards() async {
+    return Future.delayed(Duration(seconds: 1), () => []);
+  }
+}
