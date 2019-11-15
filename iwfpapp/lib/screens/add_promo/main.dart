2353776abddf1279aee51fdcf0c65a935bce10ca
tianhaoz95 <tianhaoz@umich.edu>
2019-11-14 22:30:02 -0800

    dev: stub add promo screen

--- iwfpapp/lib/screens/add_promo/main.dart
+++ iwfpapp/lib/screens/add_promo/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iwfpapp/services/credit_card.dart';
+import 'package:iwfpapp/services/status.dart';
 
 class AddPromoScreen extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class AddPromoScreen extends StatefulWidget {
 
 class _AddPromoScreen extends State<AddPromoScreen> {
   CreditCard card;
+  SubmitScreenStatus status = SubmitScreenStatus.PENDING;
 
   @override
   void didChangeDependencies() {
@@ -18,6 +20,63 @@ class _AddPromoScreen extends State<AddPromoScreen> {
     if (ModalRoute.of(context).settings.arguments != null) {
       card = ModalRoute.of(context).settings.arguments;
     }
+  }
+
+  Widget render(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+
+  Widget renderPendingContent(BuildContext context) {
+    return Container(
+      child: Text('pending'),
+    );
+  }
+
+  Widget renderDoneContent(BuildContext context) {
+    return Container(
+      child: Text('done'),
+    );
+  }
+
+  Widget renderErrorContent(BuildContext context) {
+    return Container(
+      child: Text('error'),
+    );
+  }
+
+  Widget renderLoadingContent(BuildContext context) {
+    return Container(
+      child: Text('loading'),
+    );
+  }
+
+  Widget renderUnknownContent(BuildContext context) {
+    return Container(
+      child: Text('unknown'),
+    );
+  }
+
+  Widget renderContent(BuildContext context) {
+    Widget content = renderUnknownContent(context);
+    switch (status) {
+      case SubmitScreenStatus.DONE:
+        content = renderDoneContent(context);
+        break;
+      case SubmitScreenStatus.PENDING:
+        content = renderPendingContent(context);
+        break;
+      case SubmitScreenStatus.ERROR:
+        content = renderErrorContent(context);
+        break;
+      case SubmitScreenStatus.LOADING:
+        content = renderLoadingContent(context);
+        break;
+      default:
+        content = renderUnknownContent(context);
+    }
+    return content;
   }
 
   @override
@@ -28,6 +87,7 @@ class _AddPromoScreen extends State<AddPromoScreen> {
         backgroundColor: Colors.cyan,
         title: Text('Add Promotion for ' + card.name),
       ),
+      body: renderContent(context),
     );
   }
 }
