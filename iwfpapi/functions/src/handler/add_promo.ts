b03a0000384d4c3ba27b78b84490735e5812c34a
tianhaoz95 <tianhaoz@umich.edu>
2019-11-15 21:59:21 -0800

    dev: adding promo addition api

--- iwfpapi/functions/src/handler/add_promo.ts
+++ iwfpapi/functions/src/handler/add_promo.ts
@@ -1,9 +1,22 @@
-import { noAuthMsg, notImplementedError } from "../config/consts";
+import provider from "../provider";
+import { noAuthMsg, creditCardNotExistError } from "../config/consts";
 
 function AddPromoHandler(data, context) {
   return new Promise((resolve, reject) => {
     if (context.auth) {
-      reject(notImplementedError);
+      const userUid = context.auth.uid;
+      const userRef = provider.getUserRef(userUid);
+      const cardRef = userRef.collection("cards").doc(data.cardUid);
+      cardRef.get().then((snap) => {
+        if (snap.exists) {
+          const promoUid = data.promoUid;
+          const promoRef = cardRef.collection("promos").doc(promoUid);
+        } else {
+          reject(creditCardNotExistError);
+        }
+      }).catch((err) => {
+        reject(err);
+      });
     } else {
       reject(noAuthMsg);
     }
