47b1e3835b22f2f22b43737803dba2681f712897
tianhaoz95 <tianhaoz@umich.edu>
2019-12-09 20:57:19 -0800

    fix: better wait for checks

--- iwfpapp/test_driver/app_test.dart
+++ iwfpapp/test_driver/app_test.dart
@@ -6,25 +6,12 @@ import 'utilities/screenshot.dart';
 
 void main() {
   group('iwfpapp', () {
-    // First, define the Finders and use them to locate widgets from the
-    // test suite. Note: the Strings provided to the `byValueKey` method must
-    // be the same as the Strings we used for the Keys in step 1.
-    final signInEmailInput = find.byValueKey('sign_in_email_input');
-    final signInPasswordInput = find.byValueKey('sign_in_password_input');
-    final shopNavBtn = find.byValueKey('shop_nav_btn');
-    final cardsNavBtn = find.byValueKey('cards_nav_btn');
-    final userNavBtn = find.byValueKey('user_nav_btn');
-    final contribNavBtn = find.byValueKey('contrib_nav_btn');
-    final emailSignInBtn = find.byValueKey('email_sign_in_btn');
-
     FlutterDriver driver;
 
-    // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
 
-    // Close the connection to the driver after the tests have completed.
     tearDownAll(() async {
       if (driver != null) {
         driver.close();
@@ -33,7 +20,8 @@ void main() {
 
     Future<bool> isSignedIn() async {
       try {
-        await driver.waitFor(find.text('Shop Now!'),timeout: Duration(seconds: 3));
+        await driver.waitFor(find.text('Shop Now!'),
+            timeout: Duration(seconds: 3));
         return true;
       } catch (err) {
         return false;
@@ -52,18 +40,21 @@ void main() {
         await driver.waitFor(find.text('Sign In with Email'));
       }
       await takeNamedScreenshot(driver, 'sign_in_screen_pending');
-      await driver.tap(signInEmailInput);
+      await driver.tap(find.byValueKey('sign_in_email_input'));
       await driver.enterText('tianhaoz@umich.edu');
-      await driver.tap(signInPasswordInput);
+      await driver.tap(find.byValueKey('sign_in_password_input'));
       await driver.enterText('Iwfpapp#950903');
       await takeNamedScreenshot(driver, 'sign_in_screen_presubmit');
-      await driver.tap(emailSignInBtn);
-      await driver.waitFor(find.text('Shop Now!'));
+      await driver.tap(find.text('Sign In with Email'));
+      await driver.waitFor(find.byValueKey('shop_title'));
       await takeNamedScreenshot(driver, 'home_screen');
-      await driver.tap(cardsNavBtn);
-      await driver.tap(userNavBtn);
-      await driver.tap(contribNavBtn);
-      await driver.tap(shopNavBtn);
+      await driver.tap(find.byValueKey('cards_nav_btn'));
+      await driver.waitFor(find.byValueKey('cards_title'));
+      await driver.tap(find.byValueKey('user_nav_btn'));
+      await driver.waitFor(find.byValueKey('user_title'));
+      await driver.tap(find.byValueKey('contrib_nav_btn'));
+      await driver.waitFor(find.byValueKey('contrib_title'));
+      await driver.tap(find.byValueKey('shop_nav_btn'));
     });
   });
 }
