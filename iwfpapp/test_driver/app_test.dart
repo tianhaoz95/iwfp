bd44b336ddb90561b7020f0994533febcb3302f2
Tianhao Zhou <tianhaoz@umich.edu>
2019-12-09 23:13:37 -0800

    dev: add screenshots in e2e test (#142)
    This PR adds:
    * capability to take screenshots in the end to end tests
    * capability to upload screenshots to CI builds for easy verification
    
    change list:
    * dev: add screenshots in tests
    * fix: better wait for checks

--- iwfpapp/test_driver/app_test.dart
+++ iwfpapp/test_driver/app_test.dart
@@ -2,27 +2,16 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
+import 'utilities/screenshot.dart';
+
 void main() {
   group('iwfpapp', () {
-    // First, define the Finders and use them to locate widgets from the
-    // test suite. Note: the Strings provided to the `byValueKey` method must
-    // be the same as the Strings we used for the Keys in step 1.
-    final signInEmailInput = find.byValueKey('sign_in_email_input');
-    final signInPasswordInput = find.byValueKey('sign_in_password_input');
-    final shopNavBtn = find.byValueKey('shop_nav_btn');
-    final cardsNavBtn = find.byValueKey('cards_nav_btn');
-    final userNavBtn = find.byValueKey('user_nav_btn');
-    final contribNavBtn = find.byValueKey('contrib_nav_btn');
-    final emailSignInBtn = find.byValueKey('email_sign_in_btn');
-
     FlutterDriver driver;
 
-    // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
 
-    // Close the connection to the driver after the tests have completed.
     tearDownAll(() async {
       if (driver != null) {
         driver.close();
@@ -31,7 +20,8 @@ void main() {
 
     Future<bool> isSignedIn() async {
       try {
-        await driver.waitFor(find.text('Shop Now!'),timeout: Duration(seconds: 3));
+        await driver.waitFor(find.text('Shop Now!'),
+            timeout: Duration(seconds: 3));
         return true;
       } catch (err) {
         return false;
@@ -39,9 +29,9 @@ void main() {
     }
 
     test('smoke test', () async {
+      await prepareScreenshotArea();
       bool signedIn = await isSignedIn();
       if (signedIn) {
-        print('Signed in');
         await driver.tap(find.byValueKey('user_nav_btn'));
         await driver.waitFor(find.text('Go to Authentication'));
         await driver.tap(find.text('Go to Authentication'));
@@ -49,16 +39,22 @@ void main() {
         await driver.tap(find.text('Logout'));
         await driver.waitFor(find.text('Sign In with Email'));
       }
-      await driver.tap(signInEmailInput);
+      await takeNamedScreenshot(driver, 'sign_in_screen_pending');
+      await driver.tap(find.byValueKey('sign_in_email_input'));
       await driver.enterText('tianhaoz@umich.edu');
-      await driver.tap(signInPasswordInput);
+      await driver.tap(find.byValueKey('sign_in_password_input'));
       await driver.enterText('Iwfpapp#950903');
-      await driver.tap(emailSignInBtn);
-      await driver.waitFor(find.text('Shop Now!'));
-      await driver.tap(cardsNavBtn);
-      await driver.tap(userNavBtn);
-      await driver.tap(contribNavBtn);
-      await driver.tap(shopNavBtn);
+      await takeNamedScreenshot(driver, 'sign_in_screen_presubmit');
+      await driver.tap(find.text('Sign In with Email'));
+      await driver.waitFor(find.byValueKey('shop_title'));
+      await takeNamedScreenshot(driver, 'home_screen');
+      await driver.tap(find.byValueKey('cards_nav_btn'));
+      await driver.waitFor(find.byValueKey('cards_title'));
+      await driver.tap(find.byValueKey('user_nav_btn'));
+      await driver.waitFor(find.byValueKey('user_title'));
+      await driver.tap(find.byValueKey('contrib_nav_btn'));
+      await driver.waitFor(find.byValueKey('contrib_title'));
+      await driver.tap(find.byValueKey('shop_nav_btn'));
     });
   });
 }
