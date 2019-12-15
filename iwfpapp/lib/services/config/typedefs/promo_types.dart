484eed24d55bc410a7c09880df1ec283ebf98f35
tianhaoz95 <tianhaoz@umich.edu>
2019-12-15 00:38:06 -0800

    dev: add promo type lookup

---
+++ iwfpapp/lib/services/config/typedefs/promo_types.dart
@@ -0,0 +1,20 @@
+enum CashbackPromoType {
+  /// UNIVERSAL represents the type of
+  /// cash back promotion that applies to all
+  /// transactions. For example, Chase Pay
+  /// Apple Pay, etc
+  UNIVERSAL,
+  /// SECTOR represents the type of
+  /// cash back promotions that applies
+  /// to purchases in a certain sector.
+  /// For example, gas stations,
+  /// restaurants, drag stores, etc
+  SECTOR,
+  /// BRAND represents the type of
+  /// cash back promotions that applies
+  /// to purchase from certain brands.
+  /// For example, Walmart, Target,
+  /// BestBuy, etc
+  BRAND,
+}
+
