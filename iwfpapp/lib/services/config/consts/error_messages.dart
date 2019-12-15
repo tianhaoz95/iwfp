42420eedd98ad149a5b8469af077a3f1908d3143
tianhaoz95 <tianhaoz@umich.edu>
2019-12-14 21:57:13 -0800

    dev: add all validation functions

--- iwfpapp/lib/services/config/consts/error_messages.dart
+++ iwfpapp/lib/services/config/consts/error_messages.dart
@@ -1,2 +1,17 @@
+/// Error message about promotion information validation
 final String promoIdHasSpaceErrorMessage = 'Promotion ID cannot contain space.';
+final String promoIdEmptyErrorMessage = 'Promotion ID cannot be empty.';
+final String promoNameEmptyErrorMessage = 'Promotion name cannot be empty.';
+final String promoTypeEmptyErrorMessage = 'Promotion type cannot be empty.';
+final String promoStartEmptyErrorMessage = 'Promotion start date cannot be empty.';
+final String promoEndEmptyErrorMessage = 'Promotion end date cannot be empty.';
+final String promoRepeatEmptyErrorMessage = 'Promotion repeat pattern cannot be empty.';
+final String promoRateNotIntErrorMessage = 'Promotion rate should be an integer.';
+final String promoRateEmptyErrorMessage = 'Promotion rate cannot be empty.';
+
+/// Error message about shopping category information validate
+final String categoryNameEmptyErrorMessage = 'Category name cannot be empty';
+final String categoryIdEmptyErrorMessage = 'Category ID cannot be empty';
+
+/// Error message about credit card information validation
 final String cardIdHasSpaceErrorMessage = 'Credit card ID cannot contain space.';
--- iwfpapp/lib/services/utilities/validators/category_info_validator.dart
+++ iwfpapp/lib/services/utilities/validators/category_info_validator.dart
@@ -1,4 +1,23 @@
+import 'package:iwfpapp/services/config/consts/error_messages.dart';
 import 'package:iwfpapp/services/config/typedefs/validation_response.dart';
+
+ValidationResponse isValidCategoryId(String id) {
+  ValidationResponse response = ValidationResponse(valid: true);
+  if (id.isEmpty) {
+    response.valid = false;
+    response.messages.add(categoryIdEmptyErrorMessage);
+  }
+  return response;
+}
+
+ValidationResponse isValidCategoryName(String name) {
+  ValidationResponse response = ValidationResponse(valid: true);
+  if (name.isEmpty) {
+    response.valid = false;
+    response.messages.add(categoryNameEmptyErrorMessage);
+  }
+  return response;
+}
 
 ValidationResponse isValidCategoryInfo(String name, String id) {
   ValidationResponse response = ValidationResponse(valid: true);
