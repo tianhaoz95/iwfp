9e96855720590591b91a14c580d00d2ced46970c
tianhaoz95 <tianhaoz@umich.edu>
2019-12-15 16:30:37 -0800

    chore: format code

--- iwfpapp/lib/services/config/typedefs/repeat_pattern.dart
+++ iwfpapp/lib/services/config/typedefs/repeat_pattern.dart
@@ -2,10 +2,12 @@ enum CashbackPromoRepeatPattern {
   /// CONST means the cashback promotion
   /// does not expire at all time.
   CONST,
+
   /// ANNUAL means the cashback promotion
   /// is activated every year at the same
   /// period of time.
   ANNUAL,
+
   /// UNKNOWN means the cashback promotion
   /// is not recognized. Mostly it means error
   UNKNOWN,
