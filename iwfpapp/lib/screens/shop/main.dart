749cd992e09e396873f9998495803bf85e91ccee
Tianhao Zhou <tianhaoz@umich.edu>
2019-12-21 23:20:18 -0800

    dev: add empty category indicator (#184)
    change list:
    * dev: add empty category indicator
    * dev: bump version

--- iwfpapp/lib/screens/shop/main.dart
+++ iwfpapp/lib/screens/shop/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iwfpapp/services/config/typedefs/home_tab_id.dart';
 import 'package:iwfpapp/services/config/typedefs/shop_category.dart';
 import 'package:iwfpapp/services/data_store.dart';
 import 'package:iwfpapp/widgets/category/basic.dart';
@@ -27,14 +28,64 @@ class _ShopNow extends State<ShopNow> {
       future: categories,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return Container(
-              key: Key('suggested_categories'),
-              child: Center(
-                  child: ListView(
-                children: snapshot.data.map((ShopCategory category) {
-                  return BasicCategory(category);
-                }).toList(),
-              )));
+          if (snapshot.data.isEmpty) {
+            return Container(
+              child: ListView(
+                children: <Widget>[
+                  Material(
+                    color: Colors.teal[100],
+                    child: Card(
+                      color: Colors.teal,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Center(
+                            child: Text(
+                              'No shopping sector found',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Center(
+                            child: Text(
+                              'Please consider adding cards',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    child: RaisedButton(
+                      color: Colors.green,
+                      child: Text('Go to card wallet', style: TextStyle(color: Colors.white),),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/', arguments: HomeTabId.CARD_MANAGEMENT);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Container(
+                key: Key('suggested_categories'),
+                child: Center(
+                    child: ListView(
+                  children: snapshot.data.map((ShopCategory category) {
+                    return BasicCategory(category);
+                  }).toList(),
+                )));
+          }
         } else if (snapshot.hasError) {
           return Center(
             child: Text('Error: ' + snapshot.error.toString()),
