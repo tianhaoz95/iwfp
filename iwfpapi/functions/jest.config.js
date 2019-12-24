08e84d3fe0571a8ee626e4f32695e9c2236bf0e3
tianhaoz95 <tianhaoz@umich.edu>
2019-12-23 22:10:56 -0800

    dev: add allowed files

--- iwfpapi/functions/jest.config.js
+++ iwfpapi/functions/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
+  moduleFileExtensions: ['js', 'ts', 'node'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
