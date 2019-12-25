1143e9c6515ad94cb7a9d69f259f9144a46a1d6d
tianhaoz95 <tianhaoz@umich.edu>
2019-12-24 23:54:50 -0800

    dev: upgrade auth and ios config

--- iwfpapp/lib/generated_plugin_registrant.dart
+++ iwfpapp/lib/generated_plugin_registrant.dart
@@ -3,11 +3,13 @@
 //
 import 'dart:ui';
 
+import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins(PluginRegistry registry) {
+  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   registry.registerMessageHandler();
 }
