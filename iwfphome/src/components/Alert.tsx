c2e6ed75897104c2616e1cd895d860a2ac82548a
Tianhao Zhou <tianhaoz@umich.edu>
2020-01-30 11:32:08 -0800

    dev: add home doc (#283)

---
+++ iwfphome/src/components/Alert.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { SFC } from 'react'
+
+export type Kind = 'info' | 'positive' | 'negative' | 'warning'
+export type KindMap = Record<Kind, string>
+
+const kinds: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+}
+
+export interface AlertProps {
+  /**
+   * Set this to change alert kind
+   * @default info
+   */
+  kind: 'info' | 'positive' | 'negative' | 'warning'
+}
+
+export const Alert: SFC<AlertProps> = ({ children, kind, ...rest }) => (
+  <div
+    style={{
+      padding: 20,
+      background: kinds[kind] || 'white',
+      borderRadius: 3,
+      color: 'white',
+    }}
+    {...rest}
+  >
+    {children}
+  </div>
+)
