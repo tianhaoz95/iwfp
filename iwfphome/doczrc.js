faaabf9788fc393888a66a7b864e582fca4b8952
Tianhao Zhou <tianhaoz@umich.edu>
2020-01-31 23:41:14 -0800

    docs: init skeleton (#302)

--- iwfphome/doczrc.js
+++ iwfphome/doczrc.js
@@ -1,5 +1,31 @@
 export default {
   typescript: true,
   hashRouter: true,
-  base: '/iwfp-home'
+  base: '/iwfp-home',
+  menu: [
+    'Home',
+    'Design',
+    {
+      name: 'Developer',
+      menu: [
+        'App',
+        'Server',
+        'Continuous Integration',
+        'Documentation'
+      ]
+    },
+    {
+      name: 'Contribute',
+      menu: [
+        'As User',
+        'As Developer',
+        'As Sponsor'
+      ]
+    }
+  ],
+  ignore: [
+    'README.md'
+  ],
+  title: "I Want 5% Project",
+  description: "A utility app to help credit card users maximize cashback rewards."
 }
