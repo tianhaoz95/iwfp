9c6dde41e554e22391b6af49be5851eb4fc93ede
tianhaoz95 <tianhaoz@umich.edu>
2020-04-10 13:51:47 -0700

    dev: migrate iwfpapp to app for clarity

--- app/lib/generated_plugin_registrant.dart
+++ app/lib/generated_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:firebase_core_web/firebase_core_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
