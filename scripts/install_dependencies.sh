9ce60156ae3e1f7d131764fbc4984adba36fb750
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-22 15:42:38 -0700

    dev: add flutter init into install deps script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,11 @@ set -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_ROOT="$(dirname "$SCRIPT_DIR")"
 
+flutter channel master
+flutter config --enable-web
+flutter upgrade
+flutter --version
+
 npm install -g yarn
 npm install -g prettier
 npm install -g protobufjs
