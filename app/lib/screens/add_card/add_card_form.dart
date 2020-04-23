c72ab190f3f8ebbed7ffe73fe5acdfd1bb74a2d7
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-23 00:53:05 +0000

    fix: make the linter happy

--- app/lib/screens/add_card/add_card_form.dart
+++ app/lib/screens/add_card/add_card_form.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iwfpapp/services/config/typedefs/credit_card.dart';
 import 'package:iwfpapp/services/config/typedefs/data_store.dart';
-import 'package:iwfpapp/services/config/typedefs/home_tab_id.dart';
 import 'package:iwfpapp/services/data_backend/base_data_backend.dart';
 import 'package:iwfpapp/widgets/inputs/add_card_id_input.dart';
 import 'package:iwfpapp/widgets/inputs/add_card_name_input.dart';
