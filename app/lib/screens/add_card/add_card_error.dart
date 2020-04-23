8aa40206caa512c1c1d836940e8f6a8d58c07f96
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-22 22:25:46 -0700

    fix: add timeout for error recovering (#484)
    Since the app is using Firebase as backend, usually the errors are caused by quota limitation which means that trying to recover from the error state will likely lead to another error.
    A good way is to set a count down on the error recovering screens to force the users to fetch again from the user a few seconds from the error which prevents the quota limit exception being triggered again.
    
    This PR adds count down to all the error recovering screens to make it more usable.

--- app/lib/screens/add_card/add_card_error.dart
+++ app/lib/screens/add_card/add_card_error.dart
@@ -3,8 +3,37 @@ import 'package:iwfpapp/services/data_backend/base_data_backend.dart';
 import 'package:iwfpapp/widgets/layouts/listview_item.dart';
 import 'package:provider/provider.dart';
 
-class AddCardScreenErrorContent extends StatelessWidget {
-  const AddCardScreenErrorContent();
+class AddCardScreenErrorContent extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AddCardScreenErrorContent();
+  }
+}
+
+class _AddCardScreenErrorContent extends State<AddCardScreenErrorContent> {
+  int count;
+
+  @override
+  void initState() {
+    super.initState();
+    this.count = 5;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    countDown();
+  }
+
+  Future<void> countDown() async {
+    for (int i = 0; i < 5; ++i) {
+      await Future.delayed(Duration(seconds: 1));
+      setState(() {
+        count--;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,12 +45,22 @@ class AddCardScreenErrorContent extends StatelessWidget {
             ),
           ),
           ListViewItem(
+            child: Center(
+              child: this.count == 0
+                  ? Text('Click the button below to retry')
+                  : Text(
+                      'Wait for ' + this.count.toString() + ' and try again'),
+            ),
+          ),
+          ListViewItem(
             child: RaisedButton(
                 child: Text('Try again'),
-                onPressed: () {
-                  Provider.of<DataBackend>(context, listen: false)
-                      .recoverFromError();
-                }),
+                onPressed: this.count == 0
+                    ? () {
+                        Provider.of<DataBackend>(context, listen: false)
+                            .recoverFromError();
+                      }
+                    : null),
           ),
         ],
       ),
