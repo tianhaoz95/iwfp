ff877082007327d79eb96a93276ef43691db660b
tianhaoz95 <tianhaoz@umich.edu>
2020-04-23 13:19:46 -0700

    fix: return empty list when the wallet is empty instead of error
    It seems that the remove card error was caused by a double exception thrown by the server when the wallet is empty. The empty wallet will throw a error and the refresh fetching will throw one right after that causing undefined behavior. It would also make sense to not return an error when the wallet is empty as it is complete valid state.

--- functions/src/handler/get_credit_cards.ts
+++ functions/src/handler/get_credit_cards.ts
@@ -1,5 +1,4 @@
 import { noAuthMsg } from "../config/consts";
-import { EmptyWalletError } from "../config/errors";
 import { FunctionContext } from "../config/typedefs";
 
 async function getCreditCardsHandler(data, context: FunctionContext, provider) {
@@ -9,26 +8,22 @@ async function getCreditCardsHandler(data, context: FunctionContext, provider) {
     const cardRef = userRef.collection("cards");
     const cardSnap: FirebaseFirestore.QuerySnapshot = await cardRef.get();
     const response = {};
-    if (cardSnap.empty) {
-      throw EmptyWalletError;
-    } else {
-      for (const card of cardSnap.docs) {
-        console.log("retrieve card: ", card.id, "=>", card.data());
-        response[card.id] = card.data();
-        const promoRef = cardRef.doc(card.id).collection("promos");
-        const promoSnap: FirebaseFirestore.QuerySnapshot = await promoRef.get();
-        const promos = {};
-        if (promoSnap.empty) {
-          console.log("no promo found");
-        } else {
-          console.log("promos found, retrieve promos");
-          for (const promo of promoSnap.docs) {
-            console.log("retrieved promo: ", promo.id, "=>", promo.data());
-            promos[promo.id] = promo.data();
-          }
-          response[card.id]["promos"] = promos;
-          console.log("card data and all promos retrieved");
+    for (const card of cardSnap.docs) {
+      console.log("retrieve card: ", card.id, "=>", card.data());
+      response[card.id] = card.data();
+      const promoRef = cardRef.doc(card.id).collection("promos");
+      const promoSnap: FirebaseFirestore.QuerySnapshot = await promoRef.get();
+      const promos = {};
+      if (promoSnap.empty) {
+        console.log("no promo found");
+      } else {
+        console.log("promos found, retrieve promos");
+        for (const promo of promoSnap.docs) {
+          console.log("retrieved promo: ", promo.id, "=>", promo.data());
+          promos[promo.id] = promo.data();
         }
+        response[card.id]["promos"] = promos;
+        console.log("card data and all promos retrieved");
       }
     }
     console.log("return response: ", response);
