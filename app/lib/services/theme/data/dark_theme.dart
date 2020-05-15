03b111de4aeb08a071f12669e6ccc118a48b1ff0
tianhaoz95 <tianhaoz@umich.edu>
2020-05-14 23:38:41 -0700

    dev: better promo editing UI

--- app/lib/services/theme/data/dark_theme.dart
+++ app/lib/services/theme/data/dark_theme.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
+  primaryColor: Colors.grey[900],
+  textSelectionColor: Colors.white,
 );
