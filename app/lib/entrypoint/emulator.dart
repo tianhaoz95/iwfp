30735f10fc2f41e75e803bcb06f57c03b5124585
tianhaoz95 <tianhaoz@umich.edu>
2020-05-23 20:15:43 -0700

    fix: make emulator tests happy

--- app/lib/entrypoint/emulator.dart
+++ app/lib/entrypoint/emulator.dart
@@ -1,11 +1,14 @@
 import 'package:iwfpapp/app/start_app.dart';
 import 'package:iwfpapp/services/config/typedefs/app_auth_types.dart';
+import 'package:iwfpapp/services/config/typedefs/app_theme_types.dart';
 import 'package:iwfpapp/services/config/typedefs/backend.dart';
 
 void main() {
   startApp(
     appAuthType: AppAuthType.MOCK_AUTH,
     backendType: BackendType.EMULATOR,
+    appThemeType: AppThemeType.MOCK,
     shouldReportDevCrashes: false,
+    allowDynamicLink: false,
   );
 }
