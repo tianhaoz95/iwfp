9b26b8db4d444b883ca73bb67b90df97ced34c72
Tianhao Zhou <tianhaoz@umich.edu>
2020-06-07 14:27:43 -0700

    refactor(server): simplify test suite implementation (#595)

--- functions/test/fixture/cards.ts
+++ functions/test/fixture/cards.ts
@@ -1,16 +1,22 @@
 import { CreditCard } from "../../src/interfaces/interfaces";
 import { createBasicPromotion } from "./promos";
 
-export const testCreditCard: CreditCard = CreditCard.create({
-  id: "test_card_id",
-  displayName: "test_card_name",
-});
+export function createTestCreditCard(): CreditCard {
+  const card: CreditCard = CreditCard.create({
+    id: "test_card_id",
+    displayName: "test_card_name",
+  });
+  return card;
+}
 
-export const testCreditCardTemplate: CreditCard = CreditCard.create({
-  id: "test_card_id",
-  displayName: "test_card_name",
-  promotions: [
-    createBasicPromotion(),
-    createBasicPromotion("test_promo_alternative"),
-  ],
-});
+export function createTestCreditCardTemplate(): CreditCard {
+  const template: CreditCard = CreditCard.create({
+    id: "test_card_id",
+    displayName: "test_card_name",
+    promotions: [
+      createBasicPromotion(),
+      createBasicPromotion("test_promo_alternative"),
+    ],
+  });
+  return template;
+}
