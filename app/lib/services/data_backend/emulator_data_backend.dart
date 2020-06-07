5f524aaaed67bd17823c4a471c6ab55f0a411047
Tianhao Zhou <tianhaoz@umich.edu>
2020-06-06 19:41:02 -0700

    fix: android emulator local host address

--- app/lib/services/data_backend/emulator_data_backend.dart
+++ app/lib/services/data_backend/emulator_data_backend.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:iwfpapp/services/data_backend/in_app_data_backend.dart';
 
 class EmulatorDataBackend extends InAppDataBackend {
   EmulatorDataBackend() : super() {
     // Use http://localhost:5001 for non-Android and http://10.0.2.2:5001 for Android
-    cloudFunc.useFunctionsEmulator(origin: 'http://10.0.2.2:5001');
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      cloudFunc.useFunctionsEmulator(origin: 'http://10.0.2.2:5001');
+    } else {
+      cloudFunc.useFunctionsEmulator(origin: 'http://localhost:5001');
+    }
   }
 }
