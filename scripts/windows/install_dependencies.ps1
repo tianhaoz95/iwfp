c41cc1bf457de0468fc0b1837536e64e26ec2dbf
Tianhao Zhou <tianhaoz@umich.edu>
2020-09-01 23:36:24 -0700

    dev: organize scripts

---
+++ scripts/windows/install_dependencies.ps1
@@ -0,0 +1,28 @@
+# Get project root directory
+Set-Variable -Name PROJ_ROOT -Value (get-item $PSScriptRoot).parent.FullName
+Write-Output "Use $PROJ_ROOT as project root"
+
+flutter channel master
+flutter config --enable-web
+flutter config --enable-macos-desktop
+flutter config --enable-linux-desktop
+flutter upgrade
+flutter doctor -v
+flutter --version
+
+npm install -g yarn
+npm install -g prettier
+npm install -g protobufjs
+flutter pub global activate protoc_plugin
+
+cd "${PROJ_ROOT}/app"
+flutter pub get
+cd "$PROJ_ROOT"
+
+cd "${PROJ_ROOT}/functions"
+npm install
+cd "$PROJ_ROOT"
+
+cd "${PROJ_ROOT}/app"
+yarn install
+cd "$PROJ_ROOT"
