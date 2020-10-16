9a749e873d356eb3a920669219b64ff7d288e2e4
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-15 18:38:43 -0700

    refactor: replace with script

--- scripts/configure_flutter.sh
+++ scripts/configure_flutter.sh
@@ -2,6 +2,10 @@
 
 set -o pipefail
 
+####################################
+## This should only be used in CI ##
+####################################
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_ROOT="$(dirname "$SCRIPT_DIR")"
 
@@ -10,19 +14,6 @@ flutter config --enable-web
 flutter upgrade
 flutter --version
 
-npm install -g yarn
-npm install -g prettier
-npm install -g protobufjs
-flutter pub global activate protoc_plugin
-
 cd $PROJ_ROOT/app
 flutter pub get
-cd $PROJ_ROOT
-
-cd $PROJ_ROOT/functions
-npm install
-cd $PROJ_ROOT
-
-cd $PROJ_ROOT/app
-yarn install
 cd $PROJ_ROOT
