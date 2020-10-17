39178f04325ee7009d374f434e3018a670823d01
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-17 12:03:11 -0700

    refactor: move flutter init script

--- scripts/bash/build_app_ui_catalog.sh
+++ scripts/bash/build_app_ui_catalog.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-set -e
 set -o pipefail
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJ_ROOT="$(dirname "$SCRIPT_DIR")"
 
 cd $PROJ_ROOT/app
 
