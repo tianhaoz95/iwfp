b937247761483ddc0d504679dfbaf8d84299481b
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-18 17:17:55 -0700

    chore: add some logging

--- scripts/macos/start_emulator.sh
+++ scripts/macos/start_emulator.sh
@@ -5,5 +5,6 @@ set -o pipefail
 echo "Available devices are:"
 xcrun instruments -s
 
+echo "Start device with ID ${DEVICE_ID}."
 UDID=$(xcrun instruments -s | awk -F ' *[][]' -v 'device=${DEVICE_ID}' '$1 == device { print $2 }')
 xcrun simctl boot "${UDID:?No Simulator with this name found}"
