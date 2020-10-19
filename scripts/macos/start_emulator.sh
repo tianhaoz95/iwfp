257ccb00370acca207208d29376868d460c766eb
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-18 18:04:32 -0700

    fix: wtf

--- scripts/macos/start_emulator.sh
+++ scripts/macos/start_emulator.sh
@@ -7,5 +7,5 @@ xcrun instruments -s
 
 echo "Start device with ID ${DEVICE_ID}."
 DEVICE_MATCHING="device=${DEVICE_ID}"
-UDID=$(xcrun instruments -s | awk -F ' *[][]' -v ${DEVICE_MATCHING} '$1 == device { print $2 }')
+UDID=$(xcrun instruments -s | awk -F ' *[][]' -v "${DEVICE_MATCHING}" '$1 == device { print $2 }')
 xcrun simctl boot "${UDID:?No Simulator with this name found}"
