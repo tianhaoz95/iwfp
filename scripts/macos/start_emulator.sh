9e2d003ec6ea096a5e1639d4f5a6d3a19de88fac
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-18 20:02:20 -0700

    refactor: simplify ipad integration test (#800)

--- scripts/macos/start_emulator.sh
+++ scripts/macos/start_emulator.sh
@@ -5,5 +5,7 @@ set -o pipefail
 echo "Available devices are:"
 xcrun instruments -s
 
-UDID=$(xcrun instruments -s | awk -F ' *[][]' -v 'device=${DEVICE_ID}' '$1 == device { print $2 }')
+echo "Start device with ID ${DEVICE_ID}."
+DEVICE_MATCHING="device=${DEVICE_ID}"
+UDID=$(xcrun instruments -s | awk -F ' *[][]' -v "${DEVICE_MATCHING}" '$1 == device { print $2 }')
 xcrun simctl boot "${UDID:?No Simulator with this name found}"
