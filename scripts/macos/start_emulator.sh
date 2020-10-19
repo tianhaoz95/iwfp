7d2d42bcc62c114c6ab063d14cd1ec6828d86e36
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-18 18:00:45 -0700

    fix: change it back

--- scripts/macos/start_emulator.sh
+++ scripts/macos/start_emulator.sh
@@ -6,6 +6,6 @@ echo "Available devices are:"
 xcrun instruments -s
 
 echo "Start device with ID ${DEVICE_ID}."
-DEVICE_MATCHING="\'device=${DEVICE_ID}\'"
+DEVICE_MATCHING="device=${DEVICE_ID}"
 UDID=$(xcrun instruments -s | awk -F ' *[][]' -v ${DEVICE_MATCHING} '$1 == device { print $2 }')
 xcrun simctl boot "${UDID:?No Simulator with this name found}"
