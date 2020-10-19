a9573114450b9d827b7260e4f6c594f117b42065
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-18 17:26:14 -0700

    fix: extract deep var

--- scripts/macos/start_emulator.sh
+++ scripts/macos/start_emulator.sh
@@ -6,5 +6,6 @@ echo "Available devices are:"
 xcrun instruments -s
 
 echo "Start device with ID ${DEVICE_ID}."
-UDID=$(xcrun instruments -s | awk -F ' *[][]' -v 'device=${DEVICE_ID}' '$1 == device { print $2 }')
+DEVICE_MATCHING="device=${DEVICE_ID}"
+UDID=$(xcrun instruments -s | awk -F ' *[][]' -v ${DEVICE_MATCHING} '$1 == device { print $2 }')
 xcrun simctl boot "${UDID:?No Simulator with this name found}"
