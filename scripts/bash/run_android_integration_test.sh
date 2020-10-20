d7bd7f1d8bfa412a416aceb8172e7611884ff3c4
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-19 22:33:43 -0700

    refactor: use script

---
+++ scripts/bash/run_android_integration_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o pipefail
+
+CURRENT_DIR=$(pwd)
+
+cd "$APP_ROOT"
+
+/Users/runner/Library/Android/sdk/tools/bin/avdmanager list
+sleep 20
+flutter doctor -v
+cd ./app && flutter pub get && flutter drive --target=test_driver/app.dart
+
+cd "$CURRENT_DIR"
