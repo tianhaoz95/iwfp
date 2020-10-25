a6c3b42257d94cd52850c20595b9bf200d53c688
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-25 16:12:10 -0700

    refactor: move interface to projects (#856)

--- scripts/bash/interface/generate.sh
+++ scripts/bash/interface/generate.sh
@@ -3,10 +3,10 @@
 set -o pipefail
 
 # Protobuf location
-SRC_DIR=$PROJ_ROOT/interfaces
+SRC_DIR=$PROJECTS_ROOT/interfaces
 
 # Dart code locations
-DART_DST_DIR=$PROJ_ROOT/app/lib/services/interfaces
+DART_DST_DIR=$PROJECTS_ROOT/app/lib/services/interfaces
 
 # TypeScript code locations
 TS_DST_DIR=$PROJ_ROOT/vendors/firebase/functions/src/interfaces
@@ -19,3 +19,5 @@ protoc -I $SRC_DIR --dart_out=$DART_DST_DIR $SRC_DIR/*.proto
 # rm -rf $TS_DST_DIR/interfaces.d.ts $TS_DST_DIR/interfaces.js
 pbjs -t static-module -w commonjs -o $TS_DST_DIR/interfaces.js $SRC_DIR/*.proto
 pbts --out $TS_DST_DIR/interfaces.d.ts $TS_DST_DIR/interfaces.js
+
+. "$BASH_SCRIPT_DIR/lint_workspace.sh" format
